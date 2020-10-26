947e50416057fb5174e4ac677136d278631ef1b7
Jacob Schaupp <jacob.schaupp@rockets.utoledo.edu>
2020-10-25 21:21:07 -0400

    Initial files brought over from server application

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Classes/tcp_server.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    try
+    {
+        boost::asio::io_context io_context;
+        tcp_server server(io_context);
+        io_context.run();
+    }catch(std::exception& e){
+        std::cerr << e.what() << std::endl;
+    }
+    return 0
 }
